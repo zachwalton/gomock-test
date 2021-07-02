04033d4265da8cc0d4acfb1c2307963b511916c2
Zach Walton <zachwalton@apple.com>
2021-07-01 18:02:39 -0700

    matcher example

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package gmt_test
 
 import (
+	"errors"
+	"fmt"
 	"github.com/golang/mock/gomock"
 	gmt "github.com/mattnolf/gomock-test"
 	mock "github.com/mattnolf/gomock-test/mock"
@@ -11,13 +13,41 @@ func TestStart(t *testing.T) {
 	t.Run("test the consumer runs", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 
+		errCouldNotConsume := errors.New("could not consume")
+
 		mockHandler := func(id string) error {
-			return nil
+			return errCouldNotConsume
 		}
 
 		mockConsumer := mock.NewMockConsumer(ctrl)
-		mockConsumer.EXPECT().Consume("some-name", mockHandler)
+		mockConsumer.EXPECT().Consume("some-name", newHandlerMatcher(mockHandler))
 
 		gmt.Start(mockConsumer, gmt.WithSomeName(mockHandler))
 	})
+}
+
+type handlerMatcher struct {
+	handler gmt.ConsumerHandler
+}
+
+var _ gomock.Matcher = &handlerMatcher{}
+
+// Matches implements gomock.Matcher
+func (fm *handlerMatcher) Matches(x interface{}) bool {
+	handler, ok := x.(gmt.ConsumerHandler)
+	if !ok {
+		return false
+	}
+
+	// compare output of expected handler and handler invoked by gmt.Start()
+	return fm.handler("") == handler("")
+}
+
+// String implements gomock.Matcher
+func (fm handlerMatcher) String() string {
+	return fmt.Sprintf("handler: %+v", fm.handler)
+}
+
+func newHandlerMatcher(handler gmt.ConsumerHandler) *handlerMatcher {
+	return &handlerMatcher{handler: handler}
 }
